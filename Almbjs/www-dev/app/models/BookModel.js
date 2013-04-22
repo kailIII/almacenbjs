b8e62b7b2580db2d367ae39ed07c87db965943ca
deesebc <sinliston@gmail.com>
2013-04-22 11:59:43 +0200

    Evitamos que se parsee si el objeto es undefined

--- Almbjs/www-dev/app/models/BookModel.js
+++ Almbjs/www-dev/app/models/BookModel.js
@@ -1,14 +1,22 @@
 define(["Backbone"], function(Backbone){
     var BookModel = Backbone.Model.extend({
     	parse: function(item) {
-            return {
-            	//id se tiene que llamar ID
-                id: item.id,
-                nombre: item.nombre,
-                autor: item.autor,
-                argumento: item.argumento,
-                paginas: item.paginas
-            };
+    		console.log('BookModel - parse');
+    		if(item != null){
+    			console.log('BookModel - item not null');
+    			 return {
+    	            	//id se tiene que llamar ID
+    	                id: item.id,
+    	                nombre: item.nombre,
+    	                autor: item.autor,
+    	                argumento: item.argumento,
+    	                paginas: item.paginas
+    	            };
+    		}else{
+    			console.log('BookModel - item null');
+    			return {};
+    		}
+           
         }
     });
     return BookModel;
